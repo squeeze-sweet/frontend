8c3cd88d71890e328449ee1a06c0782d640cde14
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2022-12-10 21:34:21 +0500

    fix merge error

--- src/pages/finish/index.tsx
+++ src/pages/finish/index.tsx
@@ -4,16 +4,21 @@ import styles from './step-2.module.scss';
 
 export default function Finish() {
   const [finishUrl, setFinishUrl] = useState('');
-  const { fileNames, stepsData, mergeVideos, finalVideoData, chosenAudioId, userInfo } = useStore(
-    state => ({
-      fileNames: state.filenames,
-      stepsData: state.stepsData,
-      mergeVideos: state.mergeVideos,
-      finalVideoData: state.finalVideoData,
-      chosenAudioId: state.chosenAudioId,
-      userInfo: state.userInfo,
-    }),
-  );
+  const {
+    fileNames,
+    stepsData,
+    mergeVideos,
+    finalVideoData,
+    chosenAudioId,
+    userInfo,
+  } = useStore((state) => ({
+    fileNames: state.filenames,
+    stepsData: state.stepsData,
+    mergeVideos: state.mergeVideos,
+    finalVideoData: state.finalVideoData,
+    chosenAudioId: state.chosenAudioId,
+    userInfo: state.userInfo,
+  }));
 
   const getFinalVideo = async (data, meta, chosenAudioId) => {
     await mergeVideos(data, JSON.stringify(meta), chosenAudioId);
@@ -48,17 +53,25 @@ export default function Finish() {
         });
       }
       currentTime +=
-        Math.floor(stepsData[fileName].length) - Math.floor(stepsData[fileName].fragmentStartTime);
-      getFinalVideo(files, meta, chosenAudioId);
+        Math.floor(stepsData[fileName].length) -
+        Math.floor(stepsData[fileName].fragmentStartTime);
     });
+    getFinalVideo(files, meta, chosenAudioId);
   }, []);
 
   return (
     <section className={styles.container}>
-      <h1>{!Boolean(finalVideoData) ? 'youre video is almost here'! : 'Done!'} </h1>
+      <h1>
+        {!Boolean(finalVideoData) ? 'youre video is almost here'! : 'Done!'}{' '}
+      </h1>
       {finalVideoData && (
         <>
-          <video className={styles.video} src={`${finalVideoData}`} poster='poster.jpg' controls />
+          <video
+            className={styles.video}
+            src={`${finalVideoData}`}
+            poster="poster.jpg"
+            controls
+          />
         </>
       )}
     </section>
